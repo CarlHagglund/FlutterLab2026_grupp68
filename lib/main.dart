262c714cd5b0034887db6450c018790e17c828b0
Alex Niko Nikic <alexnikic@outlook.com>
2026-04-15 16:47:25 +0200

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:lab1/model/task_handler.dart';
-import 'package:provider/provider.dart';
 import 'package:lab1/pages/main_view.dart';
-
-
+import 'package:provider/provider.dart';
+import 'package:lab1/model/task_handler.dart';
 
 void main() {
-   runApp(ChangeNotifierProvider(
-      create: (context) => TaskHandler(),
-      child: const MyApp(),
-   ));  
+  runApp(ChangeNotifierProvider(
+    create: (context) => TaskHandler(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +16,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of the app.
   @override
   Widget build(BuildContext context) {
+    // Use a MaterialApp as the skeleton
+    // for the app.
     return MaterialApp(
-      home: MainView(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      // The starting page of the app
+      home: const MainView(),
     );
   }
 }
-    
-    
-      
--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lab1/widgets/task_list.dart';
+import 'package:lab1/pages/add_view.dart';
+import 'package:lab1/model/types.dart';
+import 'package:provider/provider.dart';
+import 'package:lab1/model/task_handler.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -8,10 +12,45 @@ class MainView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Things todo"),
+        title: const Text('Things todo'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: _actions(context),
       ),
-      body: TaskList(),
+      body: const TaskList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddView(),
+            ),
+          );
+        },
+        tooltip: 'Add todo',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  List<Widget> _actions(context) {
+    return [_filterMenu(context)];
+  }
+
+  Widget _filterMenu(context) {
+    var taskHandler = Provider.of<TaskHandler>(context, listen: false);
+    return PopupMenuButton<FilterType>(
+      initialValue: taskHandler.mode,
+      onSelected: (FilterType item) {
+        taskHandler.setMode(item);
+      },
+      itemBuilder: (BuildContext context) => [
+        const PopupMenuItem<FilterType>(
+            value: FilterType.all, child: Text('All')),
+        const PopupMenuItem<FilterType>(
+            value: FilterType.done, child: Text('Done')),
+        const PopupMenuItem<FilterType>(
+            value: FilterType.undone, child: Text('Undone')),
+      ],
     );
   }
 }
